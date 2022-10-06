858d3893d32dd46c9927a655ec0e911280cb2e0b
Momin Bin Shahid <mshahid@Momins-MacBook-Pro.local>
2022-10-06 18:23:19 +0500

    try catch on crops index page

--- crops/index.ts
+++ crops/index.ts
@@ -9,34 +9,40 @@ const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  switch (req.method) {
-    
-    case "GET":
-      if(req.query.id)
-        await getCropById.default(context,req);
-      else
-        await getCrops.default(context, req);
-      break;
+  try {
+    switch (req.method) {
+      case "GET":
+        if (req.query.id) await getCropById.default(context, req);
+        else await getCrops.default(context, req);
+        break;
 
-    case "POST":
-      await addCrop.default(context, req);
-      break;
+      case "POST":
+        await addCrop.default(context, req);
+        break;
 
-    case "PUT":
+      case "PUT":
         await updateCrop.default(context, req);
-      break;
+        break;
 
-    case "DELETE":
-      break;
+      case "DELETE":
+        break;
 
-    default:
-      context.res = {
-        status: 404,
-        body: {
-          message: "Route not found.",
-        },
-      };
-      break;
+      default:
+        context.res = {
+          status: 404,
+          body: {
+            message: "Route not found.",
+          },
+        };
+        break;
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: {
+        message: error,
+      },
+    };
   }
 };
 
