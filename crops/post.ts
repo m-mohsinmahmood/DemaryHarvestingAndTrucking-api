3287d1b3e3e229fba6bd19f668e77498176c7669
Momin Bin Shahid <mshahid@Momins-MacBook-Pro.local>
2022-10-06 16:49:36 +0500

    Add crop validation

--- crops/post.ts
+++ crops/post.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { Client } from "pg";
 import { config } from "../services/database/database.config";
 import { crop } from "./model";
+import { cropValidator } from "./validator";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -9,9 +10,19 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   const db = new Client(config);
 
-  try {
+  try {    
+    //#region Validation
     const crop: crop = req.body;
-
+    const validation = cropValidator(crop);
+    let error = [];
+    let errorMessage = ``;
+    validation.details.forEach((err) => {
+      error.push(err.message);
+    });
+    errorMessage = error.join(", ");
+    if (error.length > 0) throw { message: errorMessage };
+    //#endregion
+    //#region Query Execution
     let query = `
         INSERT INTO "Crops" ("name", "variety", "bushel_weight")
         VALUES ('${crop.name}', '${crop.variety}', '${crop.bushel_weight}');
@@ -20,17 +31,19 @@ const httpTrigger: AzureFunction = async function (
     db.connect();
     await db.query(query);
     db.end();
-
+    //#endregion
+    //#region Success Response
     context.res = {
       status: 200,
       body: {
         message: "Crop has been created successfully",
       },
     };
-
     context.done();
     return;
+    //#endregion
   } catch (error) {
+    //#region Error Response
     context.res = {
       status: 500,
       body: {
@@ -38,6 +51,7 @@ const httpTrigger: AzureFunction = async function (
       },
     };
     return;
+    //#endregion
   }
 };
 
