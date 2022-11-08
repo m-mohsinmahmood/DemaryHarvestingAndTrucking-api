699b432f4b7b614152e393f47cc3739839b03376
Momin Bin Shahid <mshahid@Momins-MacBook-Pro.local>
2022-11-08 22:58:42 +0500

    Combining Rate is_deleted issue fixed

--- customer-combining-rate/get.ts
+++ customer-combining-rate/get.ts
@@ -27,7 +27,7 @@ const httpTrigger: AzureFunction = async function (
         FROM 
                 "Combining_Rates" cr 
                 INNER JOIN "Crops" c 
-                ON cr."crop_id" = c."id" AND c."is_deleted" = false
+                ON cr."crop_id" = c."id" AND cr."is_deleted" = false
 
         ${whereClause}
         ORDER BY 
