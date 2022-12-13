d257f3fedc97ece3ca30249082c448f2b04c7e1b
Momin Bin Shahid <mshahid@Momins-MacBook-Pro.local>
2022-12-13 20:14:21 +0500

    Update Reference Intervew to Reference Call Completed

--- applicants/applicant-review.ts
+++ applicants/applicant-review.ts
@@ -119,7 +119,7 @@ export function updateQuery(applicant, email, type) {
                 `;
             break;
             
-            case "Reference Interview Completed":
+            case "Reference Call Completed":
                 query = query + `
                         "status_step" = '${applicant.status_step}',
                         "status_message" = '${applicant.status_message}'
