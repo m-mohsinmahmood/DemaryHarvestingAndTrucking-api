8e662b683a32c565818ec314a640f5d2efb9df88
mohsinmahmood-cinnova <mmahmood@cinnova.com>
2023-01-19 14:32:42 +0500

    Update

--- customer-job-close-out/model.ts
+++
@@ -1,5 +0,0 @@
-export interface job_close_out{
-    date: string,
-    total_acres: number,
-    total_gps_acres: number,
-}
--- customer-job-setup/model.ts
+++ customer-job-setup/model.ts
@@ -1,35 +1,24 @@
-export interface job_setup{
-    customer_id?: string,
-    farm_id: string,
-    crop_id:string,
-    state: string,
-    field_id: string,
+export interface job_setup {
     id?: string,
-    employeeId?: string,
-    field_id_new?: string,
-    total_acres?: string,
-    total_gps_acres?: string
-    is_field_changed?: boolean,
-    has_employee?: boolean,
-    job_id?: string,
-    // customer_name:string,
-    // farm_name:string,
-    // field_name:string,
-    // crop_name: string,
+    customer_id?: string;
+    farm_id: string;
+    crop_id: string;
+    state: string;
+    field_id: string;
+    crew_chief_id?: string;
 }
-export interface job_update{
-    customer_id:string,
-    farm_id: string,
-    crop_id:string
+export interface job_update {
+    customer_id: string;
+    farm_id: string;
+    crop_id: string
 }
-export interface job_close{
-    customer_id:string,
-    employeeId:string,
-    is_close?: boolean,
-    is_close_crew?: boolean,
-    is_close_combine?: boolean,
-    is_close_kart?: boolean,
-    // date:string,
-    total_acres: number,
+export interface job_close {
+    customer_id: string;
+    employeeId: string;
+    is_close?: boolean;
+    is_close_crew?: boolean;
+    is_close_combine?: boolean;
+    is_close_kart?: boolean;
+    total_acres: number;
     total_gps_acres: number
 }
