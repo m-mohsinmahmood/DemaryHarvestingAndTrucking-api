7dba368ddc39b64e0dfa81866d034aa6f144db91
haris103 <haris.mahmood103@gmail.com>
2023-01-27 17:49:35 +0500

    maintenance & repair

---
+++ main_repair/model.ts
@@ -0,0 +1,61 @@
+export interface reportEquipmentIssue {
+    empId: string,
+    equipmentId: string,
+    city: string,
+    state: string,
+    ticketType:string,
+    repairTicketId:string,
+    issueCategory: string,
+    severityType: string,
+    status: string,
+    description: string
+}
+export interface repairTicket{
+      repairTicketId: string,
+      assignedById: string,
+      assignedToId: string,
+      equipmentId: string,
+      city: string,
+      state: string,
+      issueCategory: string,
+      severityType: string,
+      status: string,
+    //   summary: string,
+      description: string,
+      ticketType: string,
+      empId:string
+}
+export interface maintenanceTicket{
+     repairTicketId: string,
+      assignedById: string,
+      assignedToId: string,
+      equipmentId: string,
+      city: string,
+      state: string,
+      issueCategory: string,
+      severityType: string,
+      status: string,
+    //   summary: string,
+      description: string,
+      ticketType: string,
+      empId:string
+
+}
+export interface assignTicket {
+    repairTicketId: string,
+      empModule: string,
+      assignedById: string,
+      assignedToId: string,
+      equipID: string,
+      city: string,
+      state: string,
+      issueCategory: string,
+      severityType: string,
+      status: string,
+      summary: string,
+      description: string
+}
+export interface completeTicket {
+      summary: string,
+     
+}
