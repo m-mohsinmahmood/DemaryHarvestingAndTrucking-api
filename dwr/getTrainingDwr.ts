5141305eb8633cdc658f4e4c719190914488e433
Harris <you@example.com>
2023-03-19 14:37:49 -0700

    supervisor code updated

--- dwr/getTrainingDwr.ts
+++ dwr/getTrainingDwr.ts
@@ -5,10 +5,13 @@ export function GetTrainingDwr(employee_id: any, date: any, dateType: any, month
 
     let where = ``;
 
-    if (role === 'supervisor')
-        where = `${where} AND training.trainer_id = '${employee_id}'`;
-    else
-        where = `${where} AND dwr.employee_id = '${employee_id}'`;
+    // if (role === 'supervisor')
+    // {
+    //     where = `${where} AND training.supervisor_id = '${employee_id}'`;
+    //     where = `${where} AND trainee.trainer_id = '${employee_id}'`;
+    // }
+    // else
+    //     where = `${where} AND dwr.employee_id = '${employee_id}'`;
 
     if (dateType === 'month') {
         where = `${where} AND EXTRACT(MONTH FROM dwr.created_at) = '${month}'`
@@ -18,7 +21,7 @@ export function GetTrainingDwr(employee_id: any, date: any, dateType: any, month
         where = `${where} AND CAST(dwr.created_at AS Date) = '${date}'`
     }
 
-    if (operation === 'getDWR') {
+    if (operation === 'getDWR' && role !== 'supervisor') {
         getDwr = `
     select 
     DISTINCT dwr_employees."id" as dwr_id,
@@ -34,6 +37,7 @@ export function GetTrainingDwr(employee_id: any, date: any, dateType: any, month
 
     WHERE 
     dwr.is_day_closed= TRUE
+    AND dwr.employee_id = '${employee_id}'
     ${where}
     ;
     
@@ -51,10 +55,49 @@ export function GetTrainingDwr(employee_id: any, date: any, dateType: any, month
 
     WHERE 
     dwr.is_day_closed= TRUE
+    AND dwr.employee_id = '${employee_id}'
     ${where}
     ;
-    ;
 
+    `;
+    }
+    else if (operation === 'getDWR' && role === 'supervisor') {
+        getDwr = `
+    select 
+    DISTINCT dwr_employees."id" as dwr_id,
+    dwr.dwr_type,
+    dwr_employees.created_at
+    
+    from 
+    
+    "Bridge_DailyTasks_DWR" bridge 
+    INNER JOIN "DWR_Employees" dwr_employees ON bridge.dwr_id = dwr_employees."id"
+    INNER JOIN "DWR" dwr ON bridge.task_id = dwr."id"
+    INNER JOIN "Training" training ON dwr.training_record_id = training."id"
+
+    WHERE 
+    dwr.is_day_closed= TRUE
+    AND training.supervisor_id = '${employee_id}'
+    ${where}
+    ;
+    
+    select 
+    DISTINCT dwr_employees."id" as dwr_id,
+    dwr.dwr_type,
+    dwr_employees.created_at
+    
+    from 
+    
+    "Bridge_DailyTasks_DWR" bridge 
+    INNER JOIN "DWR_Employees" dwr_employees ON bridge.dwr_id = dwr_employees."id"
+    INNER JOIN "DWR" dwr ON bridge.task_id = dwr."id"
+    INNER JOIN "Trainee" trainee ON dwr.trainee_record_id = trainee."id"
+
+    WHERE 
+    dwr.is_day_closed= TRUE
+    AND trainee.trainer_id = '${employee_id}'
+    ${where}
+    ;
     `;
     }
 
