5c11ea3abf669b87df215600079b850db2a965d4
mohsinmahmood-cinnova <mmahmood@cinnova.com>
2023-05-13 20:24:31 +0500

    updated

--- customer-job-setup/getInvoicedJobs.ts
+++ customer-job-setup/getInvoicedJobs.ts
@@ -42,7 +42,7 @@ const httpTrigger: AzureFunction = async function (
         ;`;
         }
 
-        else if (role === 'Combine Operator') {
+        else if (role === 'Combine Operator' || role === 'Cart Operator') {
             query = `
             SELECT
             cjs.created_at :: "date",
