4eeff325b24d289ccc4c9f398f6e8561b7807a24
Asad Amanullah <aamanullah@cinnova.local>
2023-07-04 01:05:41 +0500

    Harvesting jobs fixes

--- customer-job-result/getCustomHarvesting.ts
+++ customer-job-result/getCustomHarvesting.ts
@@ -16,10 +16,11 @@ const httpTrigger: AzureFunction = async function (
     const fields: string = req.query.fields_id;
     const destinations_id: string = req.query.destinations_id;
     const created_at: string = req.query.created_at;
-    const sort: string = req.query.sort ? req.query.sort : `ht.created_at` ;
+    const sort: string = req.query.sort ? req.query.sort : `ht.created_at`;
     const order: string = req.query.order ? req.query.order : `desc`;
     const from_date: string = req.query.from_date;
     const to_date: string = req.query.to_date;
+    const status: string = req.query.status;
 
     let whereClause: string = ` Where ht.customer_id = '${customer_id}'`;
 
@@ -28,15 +29,17 @@ const httpTrigger: AzureFunction = async function (
     if (farms) whereClause = ` ${whereClause} AND cf."id" = '${farms}'`;
     if (crops) whereClause = ` ${whereClause} AND cc.id = '${crops}'`;
     if (created_at) whereClause = ` ${whereClause} AND  extract(year from "created_at") = '${created_at}'`;
-    if (destinations_id) whereClause = ` ${whereClause} AND cd."id" = '${destinations_id}'`; 
-    if (fields) whereClause = ` ${whereClause} AND "field".ID = '${fields}'`; 
-    if (from_date && to_date) whereClause = ` ${whereClause} AND ht.created_at > '${from_date}' AND ht.created_at < '${to_date}'`; 
+    if (destinations_id) whereClause = ` ${whereClause} AND cd."id" = '${destinations_id}'`;
+    if (fields) whereClause = ` ${whereClause} AND "field".ID = '${fields}'`;
+    if (from_date && to_date) whereClause = ` ${whereClause} AND ht.created_at > '${from_date}' AND ht.created_at < '${to_date}'`;
+    if (status) whereClause = ` ${whereClause} AND ht.ticket_status = '${status}'`;
 
 
     let info_query = `
           
     SELECT
-	ht.ID AS ID,
+    ht.delivery_ticket_name as ticket_name,
+    ht.scale_ticket_number as sl_number,
 	ht.destination AS destination,
 	ht.loaded_miles AS load_miles,
 	ht.ticket_status AS status,
@@ -68,7 +71,8 @@ FROM
   SUM(CAST(cc.bushel_weight AS NUMERIC)) AS total_net_bushels,
   SUM(CAST(ht.loaded_miles AS NUMERIC)) AS total_loaded_miles,
   SUM(CAST(field.acres AS NUMERIC)) AS total_acres,
-  COUNT(ht."id") AS total_tickets
+  COUNT(ht."id") AS total_tickets,
+  customers.customer_name
 FROM
   "Harvesting_Delivery_Ticket" ht
   INNER JOIN "Customer_Field" field ON field.ID = ht.field_id
@@ -77,12 +81,11 @@ FROM
   INNER JOIN "Customer_Farm" cf ON cf."id" = ht.farm_id
   INNER JOIN "Customer_Destination" cd ON cd."name" = ht.destination
 
-
   ${whereClause}
   AND scale_ticket_weight <> '' -- Exclude empty values
   AND scale_ticket_weight IS NOT NULL -- Exclude NULL values
 
-  GROUP BY customers.company_name ;
+  GROUP BY customers.customer_name ;
   `;
 
 
