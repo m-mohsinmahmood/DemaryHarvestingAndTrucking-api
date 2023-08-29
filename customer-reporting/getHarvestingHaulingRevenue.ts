61a513456daee66d51f05badceda76327aa6a6cf
m-mohsinmahmood <mmahmood@cinnova.com>
2023-08-29 17:05:05 +0500

    Updated Customer Reporting Query for Hauling Services

--- customer-reporting/getHarvestingHaulingRevenue.ts
+++ customer-reporting/getHarvestingHaulingRevenue.ts
@@ -37,12 +37,39 @@ const httpTrigger: AzureFunction = async function (
         INNER JOIN "Combining_Rates" cr ON cr.customer_id = cjs.customer_id  AND cr.is_deleted = FALSE 
         -- AND cjs.farm_id = cr.farm_id AND cjs.crop_id = cr.crop_id
         
-        where cjs.customer_id = '${customer_id}';
-        `;
+        where cjs.customer_id = '${customer_id}'
+        ;`;
 
-        let query = `${getHarvestingServices}`;
+        let getHaulingServices = `
+        Select 
+        
+        customer.id AS customer_id,
+        customer.customer_name AS customer_name,
+        cjs.job_setup_name AS job_name,
+        cjs.farm_id AS farm_id,
+        cf."name" AS farm_name,
+        crop.id AS crop_id,
+        crop."name" AS crop_name,
+        hr.rate_type AS rate_type,
+        cjs.crop_acres AS quantity,
+        hr.rate AS rate,
+        (cjs.crop_acres::float * hr.rate::float) AS revenue,
+        (cjs.crop_acres::float * hr.rate::float)/cjs.crop_acres::float AS revenue_per_acre
+        
+        
+        from
+        
+        "Customer_Job_Setup" cjs
+        INNER JOIN "Customers" customer ON cjs.customer_id = customer."id"
+        INNER JOIN "Customer_Farm" cf ON cjs.farm_id = cf."id" AND cf.is_deleted = FALSE 
+        INNER JOIN "Crops" crop ON cjs.crop_id = crop."id"
+        INNER JOIN "Hauling_Rates" hr ON cjs.customer_id = hr.customer_id AND hr.is_deleted = FALSE
+        
+        
+        where cjs.customer_id = '${customer_id}'
+        ;`;
 
-        console.log(query);
+        let query = `${getHarvestingServices} ${getHaulingServices}`;
 
         db.connect();
 
@@ -50,6 +77,7 @@ const httpTrigger: AzureFunction = async function (
 
         let resp = {
             harvestingServices: result[0].rows,
+            haulingServices: result[1].rows
         };
 
         db.end();
