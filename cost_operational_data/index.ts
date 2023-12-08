a546cf9548c45f74acac01509d81121a16c57124
mmahmood <mmahmood@cinnova.com>
2023-12-08 15:45:31 +0500

    GET By ID  Api created for Cost Operational Data

--- cost_operational_data/index.ts
+++ cost_operational_data/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import * as addData from "./post";
-// import * as getData from "./get";
-// import * as getDataById from "./getById";
+import * as getData from "./get";
+import * as getDataById from "./getById";
 // import * as editData from "./put";
 // import * as deleteData from "./delete";
 
@@ -11,10 +11,10 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
     try {
         switch (req.method) {
-            // case "GET":
-            //     if (req.query.id) await getDataById.default(context, req);
-            //     else await getData.default(context, req);
-            //     break;
+            case "GET":
+                if (req.query.id) await getDataById.default(context, req);
+                else await getData.default(context, req);
+                break;
 
             case "POST":
                 await addData.default(context, req);
