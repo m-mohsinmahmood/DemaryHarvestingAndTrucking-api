3e6bb2aef3b1552ab685b7357666fdf6a7024b8c
mmahmood <mmahmood@cinnova.com>
2023-12-20 15:31:13 +0500

    Added DWR Edit functionality

--- dwr_employees/model.ts
+++ dwr_employees/model.ts
@@ -3,5 +3,16 @@ export interface dwr {
     employeeId?: string;
     module?: string;
     role?: string;
-    moduleToRedirect?:string;
+    moduleToRedirect?: string;
+}
+
+export interface editDWR {
+    id?: string;
+    employeeId: string;
+    begining_day: string;
+    ending_day: string;
+    state: string;
+    status: string;
+    supervisorId: string;
+
 }
--- dwr_employees/patch.ts
+++ dwr_employees/patch.ts
@@ -1,34 +1,56 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { Client } from "pg";
 import { config } from "../services/database/database.config";
-import { dwr } from "./model";
+import { dwr, editDWR } from "./model";
 
 const httpTrigger: AzureFunction = async function (
     context: Context,
     req: HttpRequest
 ): Promise<void> {
     const db = new Client(config);
-    const dwr: dwr = req.body;
-    const moment = require('moment');
 
     try {
-        let query = `
+        const dwr: editDWR = req.body;
+        let query = ``;
+
+        if (req.body.operation == 'editDwr') {
+            query = `
+            UPDATE 
+            "DWR_Employees"
+       
+            SET 
+            "employee_id"       = '${dwr.employeeId}',
+            "begining_day"      = '${dwr.begining_day}',
+            "ending_day"        = '${dwr.ending_day}',
+            "state"             = '${dwr.state}',
+            "dwr_status"        = '${dwr.status}',
+            "supervisor_id"     = '${dwr.supervisorId}',
+            "modified_at"       = CURRENT_TIMESTAMP
+            
+            WHERE 
+            "id" = '${dwr.id}' ;`;
+        }
+
+        else {
+            const dwr: dwr = req.body;
+            query = `
         UPDATE 
         "DWR_Employees"
    
         SET 
-        "is_active" = FALSE,
+        "is_active"     = FALSE,
         "dwr_status"    = 'pendingVerification',
-        "modified_at" = CURRENT_TIMESTAMP,
-        "ending_day"  = CURRENT_TIMESTAMP
+        "modified_at"   = CURRENT_TIMESTAMP,
+        "ending_day"    = CURRENT_TIMESTAMP
         
         WHERE 
         "id" = '${dwr.id}' ;`;
 
+        }
+
         console.log(query);
 
         db.connect();
-
         let result = await db.query(query);
         db.end();
 
